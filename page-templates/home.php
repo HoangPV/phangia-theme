ea4e7d21d1607a6d5666017f70dfc568dccbbb4c
Hoang Phan <hoangpv@smartosc.com>
2021-07-01 23:07:19 +0700

    commit home page

--- page-templates/home.php
+++ page-templates/home.php
@@ -1,1 +1,16 @@
 <?php
+/**
+ * Template Name: Homepage Template
+ */
+$context = Timber::context();
+
+$timber_post     = new Timber\Post();
+$context['post'] = $timber_post;
+
+$settings = \Kenhana\PgTheme\Model\Config::get_instance();
+
+$context = array_merge($context, [
+	't_settings' => $settings
+]);
+
+Timber::render( ['homepage.twig', 'page.twig'], $context );
